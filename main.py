bc0cb2f1c7705585fb5f99ba63109bbd95cec7f7
sophiajw <sophia.wagner@t-online.de>
2023-08-01 13:44:12 +0000

    adapted to new format

--- main.py
+++ main.py
@@ -20,21 +20,39 @@ from options import Options
 from utils import save_results
 
 
+"""
+train, validate, and test a model with nested k-fold cross validation with in-domain test set and external test set.
+
+k-fold cross validation (k=5)
+[--|--|--|**|##]
+[--|--|**|##|--]
+[--|**|##|--|--]
+[**|##|--|--|--]
+[##|--|--|--|**]
+where 
+-- train
+** val
+## test
+"""
+
+
 # filter out UserWarnings from the torchmetrics package
 warnings.filterwarnings("ignore", category=UserWarning)
 
 def main(cfg):
     cfg.seed = torch.randint(0, 1000, (1, )).item() if cfg.seed is None else cfg.seed
     pl.seed_everything(cfg.seed, workers=True)
+    
+    # --------------------------------------------------------
+    # set up paths
+    # --------------------------------------------------------
 
     # saving locations
     base_path = Path(cfg.save_dir)  # adapt to own target path
-    cfg.logging_name = f'{cfg.name}_{cfg.model}_{"-".join(cfg.cohorts)}_{cfg.norm}_{cfg.target}' if not cfg.debug else 'debug'
+    cfg.logging_name = f'{cfg.name}_{cfg.model}_{"-".join(cfg.cohorts)}_{cfg.norm}_{cfg.target}' if cfg.name != 'debug' else 'debug'
     base_path = base_path / cfg.logging_name
     base_path.mkdir(parents=True, exist_ok=True)
     model_path = base_path / 'models'
-    fold_path = base_path / 'folds'
-    fold_path.mkdir(parents=True, exist_ok=True)
     result_path = base_path / 'results'
     result_path.mkdir(parents=True, exist_ok=True)
 
@@ -59,6 +77,7 @@ def main(cfg):
     test_cohorts = [train_cohorts, *cfg.ext_cohorts]
     results = {t: [] for t in test_cohorts}
 
+    # external test set
     test_ext_dataloader = []
     for ext in cfg.ext_cohorts:
         test_data, clini_info = get_multi_cohort_df(
@@ -78,22 +97,29 @@ def main(cfg):
     # --------------------------------------------------------
     # k-fold cross validation
     # --------------------------------------------------------
+    # load fold directory from data_config
+    with open(cfg.data_config, 'r') as f:
+        data_config = yaml.safe_load(f)
+        fold_path = Path(data_config[train_cohorts]['folds']) / f"{cfg.target}_{cfg.folds}folds"
+        fold_path.mkdir(parents=True, exist_ok=True)
+        
+    # split data stratified by the labels
     skf = StratifiedKFold(n_splits=cfg.folds, shuffle=True, random_state=cfg.seed)
     patient_df = data.groupby('PATIENT').first().reset_index()
     target_stratisfy = cfg.target if type(cfg.target) is str else cfg.target[0]
     splits = skf.split(patient_df, patient_df[target_stratisfy])
 
-    for l, (train_val_idxs, test_idxs) in enumerate(splits):
-        # read split from csv-file if exists already
-        if Path(fold_path / f'folds_{cfg.logging_name}_fold{l}_train.csv').exists():
-            train_idxs = pd.read_csv(fold_path / f'folds_{cfg.logging_name}_fold{l}_train.csv', index_col='Unnamed: 0').index
-            val_idxs = pd.read_csv(fold_path / f'folds_{cfg.logging_name}_fold{l}_val.csv', index_col='Unnamed: 0').index
-            test_idxs = pd.read_csv(fold_path / f'folds_{cfg.logging_name}_fold{l}_test.csv', index_col='Unnamed: 0').index
+    for k in range(cfg.folds):
+        # read split from csv-file if exists already else save split to csv
+        if Path(fold_path / f'fold{k}_train.csv').exists():
+            train_idxs = pd.read_csv(fold_path / f'fold{k}_train.csv', index_col='Unnamed: 0').index
+            val_idxs = pd.read_csv(fold_path / f'fold{k}_val.csv', index_col='Unnamed: 0').index
+            test_idxs = pd.read_csv(fold_path / f'fold{k}_test.csv', index_col='Unnamed: 0').index
         else:
-            train_idxs, val_idxs = train_test_split(train_val_idxs, stratify=patient_df.iloc[train_val_idxs][target_stratisfy], random_state=cfg.seed)
-            patient_df['PATIENT'][train_idxs].to_csv(fold_path / f'folds_{cfg.logging_name}_fold{l}_train.csv')
-            patient_df['PATIENT'][val_idxs].to_csv(fold_path / f'folds_{cfg.logging_name}_fold{l}_val.csv')
-            patient_df['PATIENT'][test_idxs].to_csv(fold_path / f'folds_{cfg.logging_name}_fold{l}_test.csv')
+            train_idxs, val_idxs = train_test_split(splits[k][0], stratify=patient_df.iloc[splits[k][0]][target_stratisfy], random_state=cfg.seed)
+            patient_df['PATIENT'][train_idxs].to_csv(fold_path / f'folds{k}_train.csv')
+            patient_df['PATIENT'][val_idxs].to_csv(fold_path / f'folds{k}_val.csv')
+            patient_df['PATIENT'][test_idxs].to_csv(fold_path / f'folds{k}_test.csv')
 
         # training dataset
         train_dataset = MILDataset(
@@ -104,23 +130,25 @@ def main(cfg):
             norm=cfg.norm,
             clini_info=cfg.clini_info
         )
-        print(f'num training samples in fold {l}: {len(train_dataset)}')
+        print(f'num training samples in fold {k}: {len(train_dataset)}')
         train_dataloader = DataLoader(
             dataset=train_dataset, batch_size=cfg.bs, shuffle=True, num_workers=int(os.environ.get('SLURM_CPUS_PER_TASK', '1')), pin_memory=True
         )
         if len(train_dataset) < cfg.val_check_interval:
             cfg.val_check_interval = len(train_dataset)
+        if cfg.lr_scheduler == 'OneCycleLR':
+            cfg.lr_scheduler_config['total_steps'] = cfg.num_epochs * len(train_dataloader)
         
         # validation dataset
         val_dataset = MILDataset(data, val_idxs, [cfg.target], norm=norm_val, clini_info=cfg.clini_info)
-        print(f'num validation samples in fold {l}: {len(val_dataset)}')
+        print(f'num validation samples in fold {k}: {len(val_dataset)}')
         val_dataloader = DataLoader(
             dataset=val_dataset, batch_size=1, shuffle=False, num_workers=int(os.environ.get('SLURM_CPUS_PER_TASK', '1')), pin_memory=True
         )
 
         # test dataset (in-domain)
         test_dataset = MILDataset(data, test_idxs, [cfg.target], norm=norm_test, clini_info=cfg.clini_info)
-        print(f'num test samples in fold {l}: {len(test_dataset)}')
+        print(f'num test samples in fold {k}: {len(test_dataset)}')
         test_dataloader = DataLoader(
             dataset=test_dataset, batch_size=1, shuffle=False, num_workers=int(os.environ.get('SLURM_CPUS_PER_TASK', '1')), pin_memory=True
         )
@@ -140,7 +168,7 @@ def main(cfg):
         # --------------------------------------------------------
         logger = WandbLogger(
             project=cfg.project,
-            name=f'{cfg.logging_name}_fold{l}',
+            name=f'{cfg.logging_name}_fold{k}',
             save_dir=cfg.save_dir,
             reinit=True,
             settings=wandb.Settings(start_method='fork'),
@@ -149,7 +177,7 @@ def main(cfg):
 
         csv_logger = CSVLogger(
             save_dir=result_path,
-            name=f'fold{l}',
+            name=f'fold{k}',
         )
 
         # --------------------------------------------------------
@@ -158,7 +186,7 @@ def main(cfg):
         checkpoint_callback = ModelCheckpoint(
             monitor='auroc/val' if cfg.stop_criterion == 'auroc' else 'loss/val',
             dirpath=model_path,
-            filename=f'best_model_{cfg.logging_name}_fold{l}',
+            filename=f'best_model_{cfg.logging_name}_fold{k}',
             save_top_k=1,
             mode='max' if cfg.stop_criterion == 'auroc' else 'min',
         )
@@ -170,50 +198,35 @@ def main(cfg):
         trainer = pl.Trainer(
             logger=[logger, csv_logger],
             accelerator='auto',
+            devices=1,
             callbacks=[checkpoint_callback],
             max_epochs=cfg.num_epochs,
             val_check_interval=cfg.val_check_interval,
             check_val_every_n_epoch=None,
-            # limit_val_batches=0.1,  # debug
-            # limit_train_batches=500,  # debug
-            # limit_val_batches=6,    # debug
-            # limit_test_batches=10,
-            # log_every_n_steps=1,  # debug
-            # fast_dev_run=True,    # debug
-            # max_steps=6,          # debug
-            enable_model_summary=False,  # debug
+            enable_model_summary=False,
         )
         
         # --------------------------------------------------------
         # training
         # --------------------------------------------------------
 
-        if Path(model_path / f'best_model_{cfg.logging_name}_fold{l}.pth').exists():
+        if Path(model_path / f'best_model_{cfg.logging_name}_fold{k}.pth').exists():
             pass
         else: 
             results_val = trainer.fit(
                 model,
                 train_dataloader,
                 val_dataloader,
-                ckpt_path=cfg.resume,
             )
             logger.log_table('results/val', results_val)
         
         # --------------------------------------------------------
         # testing
         # --------------------------------------------------------
-
-        # TODO write test function in separate file (read test split from csv files created above, load correct models for folds)
-        # TODO rewrite for multiple dataloader (problem: how to save results to csv with correct name?)
+        
         test_cohorts_dataloader = [test_dataloader, *test_ext_dataloader]
-        # test_cohorts_evaluated = []
         for idx in range(len(test_cohorts)):
-            # skip evaluation if it was already performed
-            # if Path(result_path / f'fold{l}' / f'outputs_{test_cohorts[idx]}.csv').exists():
-            #     pass
-            # else: 
             print("Testing: ", test_cohorts[idx])
-            # test_cohorts_evaluated.append(test_cohorts[idx])
             results_test = trainer.test(
                 model,
                 test_cohorts_dataloader[idx],
@@ -221,12 +234,7 @@ def main(cfg):
             )
             results[test_cohorts[idx]].append(results_test[0])
             # save patient predictions to outputs csv file
-            model.outputs.to_csv(result_path / f'fold{l}' / f'outputs_{test_cohorts[idx]}.csv')
-        
-        # remove results entries for not evaluated test cohorts
-        # for test_cohort in test_cohorts:
-        #     if test_cohort not in test_cohorts_evaluated:
-        #         results.pop(test_cohort)
+            model.outputs.to_csv(result_path / f'fold{k}' / f'outputs_{test_cohorts[idx]}.csv')
             
         wandb.finish()  # required for new wandb run in next fold
         torch.cuda.empty_cache()
@@ -245,10 +253,7 @@ if __name__ == '__main__':
     # Update the configuration with the values from the argument parser
     for arg_name, arg_value in vars(args).items():
         if arg_value is not None and arg_name != 'config_file':
-            config[arg_name]['value'] = getattr(args, arg_name)
-    
-    # Create a flat config file without descriptions
-    config = {k: v['value'] for k, v in config.items()}
+            config[arg_name] = getattr(args, arg_name)
 
     print('\n--- load options ---')
     for name, value in sorted(config.items()):
